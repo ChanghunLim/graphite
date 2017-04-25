a219bfcf4977c236916fea2fa208fbde0c814abe
Changhun Lim <lim0116@gmail.com>
2017-04-25 10:36:42 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+/* afdsafjkdslafjdksla;fjdksla;fjdksal;jfkdsl;afjdksl;afjkdsl;afjkdsl;afjkdsal;fjsdkla;f  afdsafjkdslafjdksla;fjdksla;fjdksal;jfkdsl;afjdksl;afjkdsl;afjkdsl;afjkdsal;fjsdkla;f  afdsafjkdslafjdksla;fjdksla;fjdksal;jfkdsl;afjdksl;afjkdsl;afjkdsl;afjkdsal;fjsdkla;f  afdsafjkdslafjdksla;fjdksla;fjdksal;jfkdsl;afjdksl;afjkdsl;afjkdsl;afjkdsal;fjsdkla;f
+*/
 function CWE_398() { // IDENTICAL_BRANCHES
     if (x >= 0) {
         y = x;
